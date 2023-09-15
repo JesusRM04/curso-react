3cf12ba108e129d6a28aaca92e1c223d42075878
JesusRM04 <jaromomendez@gmail.com>
2023-09-14 18:33:21 -0600

    commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Nav } from "./components/Nav"
+import { NavItem } from "./components/NavItem"
+
+
+
+
+function App() {
+  
+
+  return (
+    <>
+      <Nav></Nav>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NavItem.jsx
@@ -0,0 +1,7 @@
+import "./NavItem.css";
+
+export function NavItem(props){
+    return <div className="NavItem">
+        {props.nombre}
+    </div>
+}
